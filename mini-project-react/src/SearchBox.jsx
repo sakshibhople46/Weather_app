f7b26f506b1e52f80cbbff9bde7a594a3b9e84f4
sakshibhople46 <sakshibhople46@gmail.com>
2026-05-19 12:05:00 +0530

    Updated SearchBox and project files

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -9,6 +9,9 @@ export default function SearchBox({updateInfo}){
     let [city,setCity]=useState("");
     let [error,setError]=useState(false);
 
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const API_URL = import.meta.env.VITE_API_URL;
+
 
     let getWeatherInfo= async()=>{
         try {
